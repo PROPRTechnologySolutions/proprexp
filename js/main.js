3e2c353bebb830a2c594371da4d3fa6e4e307e54
jay <jared.x.price@gmail.com>
2017-09-08 16:15:27 -0400

    Small formatting changes for the text box on the home screen and the main logo.

--- js/main.js
+++ js/main.js
@@ -90,8 +90,8 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["We take people places they have never seen. We draw maps to new destinations.", "We build bridges between ideas and technologies.", "We introduce new perspectives.", "Take an unforgettable journey with us.", "" ],
-        typeSpeed: 100,
+        strings: ["We take people places.","We draw maps to new destinations.", "We build bridges between ideas and technologies.", "We introduce new perspectives.", "Take an unforgettable journey with us." ],
+        typeSpeed: 5,
         backDelay: 900,
         // loop
         loop: true
