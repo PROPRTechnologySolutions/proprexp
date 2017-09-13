d8a650c6130af0f3da3fb32bbcda15b945126935
jay <jared.x.price@gmail.com>
2017-09-13 00:12:44 -0400

    Cleaning things up.

---
+++ js/panorama.js
@@ -0,0 +1,53 @@
+pannellum.viewer('merwinDam', {
+    "type": "equirectangular",
+    "autoLoad" : false,
+    "vaov": "140",
+    "setPitch": "-40",
+    "maxPitch": "-5",
+    "autoRotate": "-5",
+    
+    "autoRotateInactivityDelay": "1000",
+    "hfov": "70",
+    
+    "compass": true,
+    "maxLevel": "9",
+    "minLevel": "0",
+
+
+    "panorama": "https://s3.amazonaws.com/propr/panoramas/quarterSize/merwinDamArielWA98603-CC.jpg"
+}); 
+pannellum.viewer('percivalLanding', {
+    "type": "equirectangular",
+    "autoLoad" : false,
+    "vaov": "140",
+    "setPitch": "-40",
+    "maxPitch": "-5",
+    "autoRotate": "-5",
+    
+    "autoRotateInactivityDelay": "1000",
+    "hfov": "70",
+    
+    "compass": true,
+    "maxLevel": "9",
+    "minLevel": "0",
+    
+    "panorama": "https://s3.amazonaws.com/propr/panoramas/quarterSize/301ColumbiaStreetNWOlympiaWA98501-CC.jpg"
+});
+
+pannellum.viewer('broadStreet', {
+    "type": "equirectangular",
+    "autoLoad" : false,
+    "vaov": "140",
+    "setPitch": "-40",
+    "maxPitch": "-5",
+    "autoRotate": "-5",
+    
+    "autoRotateInactivityDelay": "1000",
+    "hfov": "70",
+    
+    "compass": true,
+    "maxLevel": "9",
+    "minLevel": "0",
+
+    "panorama": "https://s3.amazonaws.com/propr/panoramas/quarterSize/BroadStreet%26LibertyStreetBloomfieldNJ07003-25m-CC.jpg"
+});
