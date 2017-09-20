e9f63e95f36d9b8a3c7bed6dae7ad261852dbb97
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2017-09-20 19:15:19 -0400

    use minifier and cache control

--- sync.sh
+++ sync.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-aws --profile propr s3 sync --exclude '.DS_Store/*' --exclude 'sync.sh' --exclude '.git/*' . s3://propr.tech/expshrunk
+html-minifier --output index.html index.big.html
+
+aws --profile propr s3 sync \
+  --exclude '.DS_Store/*' --exclude 'sync.sh' --exclude '.git/*' \
+  --cache-control 'max-age=86400' \
+  . s3://propr.tech/expshrunk
 
 # TODO
 
